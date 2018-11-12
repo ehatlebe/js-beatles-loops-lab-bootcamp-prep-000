4cd7adefcd6eea5ced4ef7fff03abedcdf5b9236
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-12 07:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  let counter = [];
+	for (var i = 0; i < musicians.length; i++) {
+		counter[i] = `${musicians[i]} plays ${instruments[i]}`;
+	};
+  return counter;
+};
+theBeatlesPlay();
+
+function johnLennonFacts() {
+  let facts = arguments[0];
+  let enthusiasticFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    enthusiasticFacts[i] = `${facts[i]}!!!`;
+    i++;
+  };
+  return enthusiasticFacts;
+};
+johnLennonFacts();
+
+function iLoveTheBeatles() {
+
+};
+iLoveTheBeatles();
